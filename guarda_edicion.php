bde048c5e24f3a5ffe1c4880e4f65c67c2bd48f3
val434 <60700334+val434@users.noreply.github.com>
2021-06-24 16:36:24 -0500

    Create guarda_edicion.php

---
+++ guarda_edicion.php
@@ -0,0 +1,33 @@
+
+<?php
+/*Recibe los valores del formulario form_libros y los imprime*/
+
+$titulo = $_POST['titulo'];
+$edicion = $_POST['edicion'];
+$editorial = $_POST['editorial'];
+$autor = $_POST['autor'];
+
+
+$db = pg_connect("host=localhost port=5432 dbname=biblioteca user=hinata password=kageyama");
+
+if ($db){
+
+    $query = "UPDATE libros WHERE idLibro='".$id."'";
+    
+
+    
+}
+
+$result = pg_query($db, $query);
+
+//imprime los valores del formulario
+echo "Titulo del libro: ".$titulo;
+echo "</br>";
+echo "Edicion: ".$edicion;
+echo "</br>";
+echo "Editorial: ".$editorial;
+echo "</br>";
+echo "Autor: " .$autor;
+pg_close($db);
+?>
+<a href = "index.php">Regresar al inicio</a>
