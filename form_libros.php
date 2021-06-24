6ea94f1c328872275c84880efb89ba3efd2a37ef
val434 <60700334+val434@users.noreply.github.com>
2021-06-24 18:16:46 -0500

    Update form_libros.php

--- form_libros.php
+++ form_libros.php
@@ -6,6 +6,19 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+<?php
+	$db= pg_connect("hostname=localhost 
+    port=5432 dbname= biblioteca user=hinata password=kageyama")
+or die ('Could not connect: ' . pg_last_error());
+if($db){
+    $query = "SELECT * FROM editorial ";
+    $query = "SELECT * FROM autor ";
+    $query = pg_query($db,$query);
+    $result = pg_fetch_assoc($query);
+
+
+}
+?>
 	<div id="wrapper">
 	<div class="form-container">
 	<span class="form-heading">Registrar nuevo libro</span>
@@ -24,15 +37,47 @@
 	</div>
 	
 
-	<div class="input-group">
-		<i class="fas fa-user"></i>
-		<input type="text" name="editorial" placeholder="Editorial"><br/>
-	</div>
+	
+  
+		<div class="form-field">
+			<select id="editorial">
+				<option value="">Editorial</option>
+			<?php	
+				if($result->num_rows > 0){
+					while($row = $result->fetch_assoc()){
+						echo '<option value="".$row['idEditorial'].'">'.$row['nombreEditorial'].'</option>';
+					}
+				}else{
+					echo '<option value=""> Editorial no disponible</option>;
+				}
+				
+				?>
+			</select>
+		
+		</div>
+		
+		<div class="form-field">
+			<select id="editorial">
+				<option value="">Editorial</option>
+			<?php	
+			$query = "SELECT * FROM autor ";
+ 	  	 	$query = pg_query($db,$query);
+  	    	        $result = pg_fetch_assoc($query);
+				if($result->num_rows > 0){
+					while($row = $result->fetch_assoc()){
+						echo '<option value="".$row['idAutor'].'">'.$row['nombreAutor'].'</option>';
+					}
+				}else{
+					echo '<option value=""> Autor no disponible</option>;
+				}
+				
+				?>
+			</select>
+		
+		</div>
+		
 
-	<div class="input-group">
-		<i class="fas fa-user"></i>
-		<input type="text" name="autor" placeholder="Autor"><br/>
-	</div>
+	
 
 	<div class="input-group">
 		<button>
@@ -44,5 +89,6 @@
 	<a href="index.php">Regresar al inicio </a>
  <br/>
 	</div>
+
 </body>
 
