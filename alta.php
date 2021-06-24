8130b63e7b97181609dd504a0ef29787d4ef8527
val434 <60700334+val434@users.noreply.github.com>
2021-06-24 14:25:25 -0500

    Update alta.php

--- alta.php
+++ alta.php
@@ -2,20 +2,27 @@
 <?php
 /*Recibe los valores del formulario form_libros y los imprime*/
 
-$db = pg_connect("host=localhost port=5432 dbname=biblioteca user=hinata password=kuroo");
+$titulo = $_POST['titulo'];
+$edicion = $_POST['edicion'];
+$editorial = $_POST['editorial'];
+$autor = $_POST['autor'];
 
 
-$query = "INSERT INTO libros VALUES ('$_POST[nombreLibro]','$_POST[edicion]',
-'$_POST[editorial]','$_POST[nombreAutor]','$_POST[apaterno]',
-'$_POST[amaterno]')";
-$result = pg_query($query);
+$db = pg_connect("host=localhost port=5432 dbname=biblioteca user=hinata password=kageyama");
+
+
+$query = "INSERT INTO libros VALUES ('".$titulo."','".$edicion."');
+
+$result = pg_query($db, $query);
 
 //imprime los valores del formulario
-echo "Nombre del libro: ".$nombreLibro;
+echo "Titulo del libro: ".$titulo;
+echo "</br>";
+echo "Edicion: ".$edicion;
 echo "</br>";
 echo "Editorial: ".$editorial;
 echo "</br>";
-echo "Autor: " .$nombreAutor. " ".$apaterno." ".$amaterno;
-
+echo "Autor: " .$autor;
+pg_close($db);
 ?>
 <a href = "index.php">Regresar al inicio</a>
