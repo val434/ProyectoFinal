1d5c527263eabd34b4e53158c036694bdc5f0d33
val434 <60700334+val434@users.noreply.github.com>
2021-06-24 14:15:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html>
+<head>
+    Biblioteca
+</head>
 <body>
 <?php
-session_start();
-echo "Bienvenido";
-echo $_SESSION[''] = ;
+    echo '<p> Bienvenido </p></br>';
+    echo '<a href="form_libros.php></a>"';
+    echo '<a href="form_edita.php></a>"';
+    echo '<a href="form_elimina.php></a>"';
 
+    $dbconn= pg_connect("hostname=localhost 
+    port=5432 dbname= biblioteca user=hinata password=kageyama")
+    or die ('Could not connect: ' . pg_last_error());
+
+    $query = "SELECT * FROM libros ORDER BY idLibro";
+    $result = pg_query($query) or die ('Query failed: ' . pg_last_error());
+
+    echo "<table>\n";
+    while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)){
+            $idLibro
+
+    }
+    echo "</table>\n";
+    pg_free_result($result);
+    pg_close($dbconn);
 
 ?>
 </body>
 </html>
-<?php
-    $query = "SELECT * from LIBROS";
-    $resp = pg_fetch_array($result);
-    pg_close($con);
-?>
