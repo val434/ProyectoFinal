dd04b80daefb68567b09fb537216a4758588e00d
val434 <60700334+val434@users.noreply.github.com>
2021-06-24 16:58:53 -0500

    Update elimina_libro.php

--- elimina_libro.php
+++ elimina_libro.php
@@ -2,22 +2,22 @@
 
 $titulo=$_POST['titulo'];
 $edicion=$_POST['edicion'];
-
-
+$editorial=$_POST['editorial'];
 $autor=$_POST['autor'];
 
 $db = pg_connect( "host=localhost port=5432 dbname=biblioteca user=hinata password=kageyama") or die (pg_last_error());
+ 
+if($db){
+  $query = "DELETE FROM libros WHERE $idLibro =".$id;
+  $query = pg_query($db,$query);
+  $resultado = pg_fetch_assoc($query);
+  if($query){
+    echo "<p> El libro se eliminó exitosamente</p>";
+  }
+}
 
-$query = "DELETE FROM libros WHERE $idLibro = ";
-$result = pg_query($db,$query);
 
-echo "Titulo del libro: ".$titulo;
-echo"</br>";
-echo "Edicion: ".$edicion;
-echo"</br>";
-echo "Editorial: ".$editorial;
-echo "</br>";
-echo "Autor: ".$autor;
+
 pg_close($db);
 ?>
 <a href = "index.php">Regresar al inicio</a>
